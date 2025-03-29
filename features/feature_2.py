49d25d11d968bcb2f6e529e71eed28704c7ad5de
Pablo González Monfort <pablohalcoguepard@gmail.com>
2025-03-29 12:49:19 +0100

    update feature1

--- features/feature_2.py
+++ features/feature_2.py
@@ -5,36 +5,39 @@ from settings_manager import SettingsManager
 
 settings = SettingsManager.get_settings()
 
-MODELS = settings["models"]["feature_2"]
+MODELS1 = settings["models"]["feature_1"]
+MODELS2 = settings["models"]["feature_2"]
 
-def run_replicate_model(model: str, input: dict):
+def run_replicate_model(model_str: str, input: dict):
     """
     Runs a specified model from Replicate with the given input parameters.
 
     Parameters
     ----------
-    model : str
+    model_str : str
         The name of the model to run.
     input : dict
         Input parameters for the model.
 
     Returns
     -------
-    Image or list[Image]
+    list[Image]
         The generated image(s) as PIL Image objects.
     """
-    if model not in MODELS:
-        raise ValueError(f"Model {model} is not supported.")
+
+    if model_str not in MODELS1 and model_str not in MODELS2:
+        raise ValueError(f"Model '{model_str}' is not available.")
+    
+    # Get the latest version of the model
+    model = replicate.models.get(model_str)
+    version = model.versions.list()[0]
     
     result = replicate.run(
-        model,
+        f"{model_str}:{version.id}",
         input=input
     )
 
-    if MODELS[model]["multiple_outputs"]:
-        # Convert all images in the output to PIL Image objects
+    if type(result) == list:
         return [Image.open(BytesIO(image.read())) for image in result]
     else:
-        # Convert the single image in the output to a PIL Image object
-        image_bytes = result.read()
-        return Image.open(BytesIO(image_bytes))
+        return [Image.open(BytesIO(result.read()))]
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from settings_manager import SettingsManager
-from features.feature_1 import train_dreambooth_model
+from features.feature_1 import train_dreambooth_model, create_replicate_model
 from features.feature_2 import run_replicate_model
 
 
@@ -10,16 +10,18 @@ REPLICATE_API_KEY = settings.get("replicate_api_key", None)
 os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_KEY
 
 if __name__ == "__main__":
-    # model = list(settings["models"]["feature_2"].keys())[1]
+
+    # model = list(settings["models"]["feature_1"].keys())[0]
+    # print(model)
 
     # input = {
-    #     "prompt": "A fantasy landscape with mountains and a river"
+    #     "prompt": "A photo of a TOKPG laying on a wooden rustic table."
     # }
     # output = run_replicate_model(model, input)
-    # output.show()
+    # output[0].show()
 
-    model_name = "flux-pingpong2"
+    model_name = "flux-pingpong3"
     images_path = "lora_dreambooth_tmp/Prueba_LoRA.zip"
-    trigger_word = "TOKPG"
-    description = "Flux fine-tuned with LoRA dreambooth conditioned on a pingpong ball case. Use the trigger word 'TOKPG' to generate images with this model."
+    trigger_word = "TOK"
+    description = "Flux fine-tuned with LoRA dreambooth conditioned on a pingpong ball case. Use the trigger word 'TOK' to generate images with this model."
     training = train_dreambooth_model(model_name, images_path, trigger_word, description)
