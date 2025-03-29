b12cd68f9ff9cc77236fa8b3e76c4ce15e354257
Pablo González Monfort <pablohalcoguepard@gmail.com>
2025-03-29 13:31:04 +0100

    main

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_KEY
 
 if __name__ == "__main__":
 
-    # model = list(settings["models"]["feature_1"].keys())[0]
-    # print(model)
+    model = list(settings["models"]["feature_1"].keys())[1]
+    print(model)
 
-    # input = {
-    #     "prompt": "A photo of a TOKPG laying on a wooden rustic table."
-    # }
-    # output = run_replicate_model(model, input)
-    # output[0].show()
+    input = {
+        "prompt": "A photo of a TOK on top of a rock in the middle of the forest, advertising, highly detailed, 8k, photorealistic",
+    }
+    output = run_replicate_model(model, input)
+    output[0].show()
 
-    model_name = "flux-pingpong3"
-    images_path = "lora_dreambooth_tmp/Prueba_LoRA.zip"
-    trigger_word = "TOK"
-    description = "Flux fine-tuned with LoRA dreambooth conditioned on a pingpong ball case. Use the trigger word 'TOK' to generate images with this model."
-    training = train_dreambooth_model(model_name, images_path, trigger_word, description)
+    # model_name = "flux-pingpong3"
+    # images_path = "lora_dreambooth_tmp/Prueba_LoRA.zip"
+    # trigger_word = "TOK"
+    # description = "Flux fine-tuned with LoRA dreambooth conditioned on a pingpong ball case. Use the trigger word 'TOK' to generate images with this model."
+    # training = train_dreambooth_model(model_name, images_path, trigger_word, description)
